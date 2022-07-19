9272fa4d5dfaac033994100b86e58009c7c9904e
keizercode <luqman.arohim@if.uai.ac.id>
2022-07-19 23:02:49 +0700

    commit 16th

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,7 +2,7 @@
 const defaultSEOConfig = {
   defaultTitle: "WA Helper",
   description: "Send Whastapp message without saving the contact first.",
-  canonical: "https://bantuwa.netlify.app/",
+  canonical: "https://bantuwa.netlify.app",
   openGraph: {
     url: "https://bantuwa.netlify.app",
     title: "WA Helper",
@@ -10,7 +10,7 @@ const defaultSEOConfig = {
     type: "website",
     images: [
       {
-        url: "https://og.yehezgun.com/api/base?description=Send%20WhatsApp%20message%20without%20saving%20the%20contact%20first.&logo=https%3A%2F%2Fcdn-icons-png.flaticon.com%2F512%2F408%2F408708.png%3Fw%3D740&logoHeight=600&logoWidth=800&siteName=bantuwa.netlify.app&templateTitle=WhatsApp%20Helper&theme=dark",
+        url: "https://og.yehezgun.com/api/base?description=Send%20WhatsApp%20message%20without%20saving%20the%20contact%20first.&logo=https%3A%2F%2Fcdn-icons-png.flaticon.com%2F512%2F408%2F408708.png%3Fw%3D740&siteName=bantuwa.netlify.app&templateTitle=WhatsApp%20Helper&theme=dark",
         alt: "bantuwa.netlify.app whatsapp-helper",
         width: 800,
         height: 600,
