6ac5b15c3f5c7485225e7fe8a32547be9b026227
keizercode <luqman.arohim@if.uai.ac.id>
2022-07-19 14:34:40 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# WA Helper
+
+Just a simple web app to help you initiate and send the WhatsApp chat without saving the phone number.
+
+## What I'm use here?
+
+- [Next.js](https://nextjs.org/) as the main tech stack and code base.
+- [Tailwind CSS](https://tailwindcss.com/) as the CSS utility class tool.
+
+## How to use it?
+
+- Open
+- You can insert the phone number that you want to send the message or just connect through WhatsApp.
+- Click the "generate" button if you want to directly go to the WhatsApp app or click "copy link" if you just want to copy the generated url.
+
+## References
+
+- [Tailwind CSS official docs](https://tailwindcss.com/)
+- [Create WA Link](https://create.wa.link/) as the main reference.
---
+++ src/components/layouts/FooterComponent.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { GiPayMoney } from "react-icons/gi";
+import { SiKofi } from "react-icons/si";
+
+import clsxm from "@/lib/helpers/clsxm";
+
+import UnderlineLink from "../links/UnderlineLink";
+import UnstyledLink from "../links/UnstyledLink";
+
+const FooterComponent = () => {
+  return (
+    <footer
+      className={clsxm(
+        "mx-auto max-w-4xl py-16 px-4 md:px-1",
+        "flex flex-wrap items-center justify-center gap-2 sm:justify-between"
+      )}
+    >
+      <div
+        className={clsxm(
+          "flex flex-wrap items-end justify-center",
+          "gap-6 md:justify-start"
+        )}
+      >
+        <UnderlineLink
+          href="https://yehezgun.com"
+          className="cursor-newtab font-semibold hover:underline"
+        >
+          {new Date().getFullYear()} | Yehezkiel Gunawan
+        </UnderlineLink>
+        <UnstyledLink href="https://www.producthunt.com/posts/wa-helper?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-wa&#0045;helper">
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=341899&theme=light"
+            alt="WA&#0032;Helper - Send&#0032;the&#0032;WhatsApp&#0032;message&#0032;without&#0032;saving&#0032;the&#0032;phone&#0032;number | Product Hunt"
+            className="w-32 md:w-40"
+          />
+        </UnstyledLink>
+      </div>
+      <div className="inline-flex gap-6">
+        <UnderlineLink href="https://ko-fi.com/yehezgun" className="gap-1">
+          <SiKofi size={20} /> Support me on Ko-fi
+        </UnderlineLink>
+        <UnderlineLink href="https://saweria.co/yehezgun" className="gap-1">
+          <GiPayMoney size={20} /> Support me on Saweria
+        </UnderlineLink>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterComponent;
