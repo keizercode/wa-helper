a65bc31033405599a3e21fc040584ddb6a13dec1
keizercode <luqman.arohim@if.uai.ac.id>
2022-07-19 22:55:04 +0700

    commit 15th

---
+++ next-seo.config.js
@@ -0,0 +1,38 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  defaultTitle: "WA Helper",
+  description: "Send Whastapp message without saving the contact first.",
+  canonical: "https://bantuwa.netlify.app/",
+  openGraph: {
+    url: "https://bantuwa.netlify.app",
+    title: "WA Helper",
+    description: "Send Whastapp message without saving the contact first.",
+    type: "website",
+    images: [
+      {
+        url: "https://og.yehezgun.com/api/base?description=Send%20WhatsApp%20message%20without%20saving%20the%20contact%20first.&logo=https%3A%2F%2Fcdn-icons-png.flaticon.com%2F512%2F408%2F408708.png%3Fw%3D740&logoHeight=600&logoWidth=800&siteName=bantuwa.netlify.app&templateTitle=WhatsApp%20Helper&theme=dark",
+        alt: "bantuwa.netlify.app whatsapp-helper",
+        width: 800,
+        height: 600,
+      },
+    ],
+    site_name: "bantuwa.netlify.app",
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "https://cdn-icons-png.flaticon.com/512/408/408708.png?w=740",
+    },
+    {
+      rel: "manifest",
+      href: "/manifest.json",
+    },
+  ],
+};
+
+export default defaultSEOConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "@/styles/globals.css";
-// import { DefaultSeo } from "next-seo";
+import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "@fontsource/catamaran";
 
-// import defaultSEOConfig from "../../next-seo.config";
+import defaultSEOConfig from "../../next-seo.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
       </Head>
-      {/* <DefaultSeo {...defaultSEOConfig} /> */}
+      <DefaultSeo {...defaultSEOConfig} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
